430a5df40811c36c03fe17c1a3c3eb1ae592e129
Ahmedsami2hvuv <reozaki.1211@gmail.com>
2026-03-08 14:21:17 +0300

    Update room_2p.py

--- handlers/room_2p.py
+++ handlers/room_2p.py
@@ -692,8 +692,6 @@ async def send_or_update_game_ui(room_id, bot, user_id, remaining_seconds=None, 
 
         controls = []
         if is_my_turn:
-            if room_id in auto_draw_tasks:
-                controls.append(InlineKeyboardButton(text="➡️ مرر الدور", callback_data=f"pass_{room_id}"))
             if len(hand) == 2 and any(check_validity(c, room['top_card'], room['current_color']) for c in hand):
                 controls.append(InlineKeyboardButton(text="🚨 اونو!", callback_data=f"un_{room_id}"))
 
